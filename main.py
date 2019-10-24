633609342d84ac91034cd48052a6c72589a7791e
NyanChanMeow <NyanChan233@protonmail.com>
2019-10-24 19:31:52 +0800

    fix: sort not working

--- main.py
+++ main.py
@@ -201,6 +201,7 @@ if (__name__ == "__main__"):
 	if (options.import_file and CONFIG_LOAD_MODE == 0):
 		IMPORT_FILENAME = options.import_file
 		sc.colors = RESULT_IMAGE_COLOR
+		sc.sortMethod = SORT_METHOD if SORT_METHOD else ""
 		sc.import_and_export(IMPORT_FILENAME)
 		sys.exit(0)
 
