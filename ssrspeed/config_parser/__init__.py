38c5d1c1ad1e9fc9f388bd22346fe9addc9a86cd
NyanChanMeow <ny@rbq.li>
2020-06-23 18:40:47 +0800

    trojan parser #105

--- ssrspeed/config_parser/__init__.py
+++ ssrspeed/config_parser/__init__.py
@@ -4,5 +4,6 @@ from .base_parser import BaseParser
 from .shadowsocks_parser import ShadowsocksParser
 from .shadowsocksr_parser import ShadowsocksRParser
 from .v2ray_parser import V2RayParser
+from .trojan_parser import TrojanParser
 
 from .config_parser import UniversalParser
